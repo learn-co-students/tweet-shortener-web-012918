e2598dfb75c96d7c7d5a1db2328ecd2f840d29d4
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-18 04:55:15 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+end
+
+def word_substituter(string)
+  array = string.split(" ")
+
+  return_array = []
+  array.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      return_array << dictionary[word.downcase]
+    else
+      return_array << word
+    end
+  end
+
+  return return_array.join(" ")
+end
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  return tweet.length <= 140 ? tweet : word_substituter(tweet)
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length <= 140
+    return tweet
+  else
+    new_tweet = word_substituter(tweet)
+    return "#{new_tweet[0...136]} ..."
+  end
+end
